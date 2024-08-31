90368637e23bf75d1a4e15a9a817cbf938436697
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-08-31 13:36:06 -0600

    [nami] Added allocator macros, default prompt

--- include/nami/nami.h
+++ include/nami/nami.h
@@ -11,7 +11,41 @@
 
 // Standard library
 #include <stdio.h>
+#include <stdlib.h>
 
 // nami 
 #include <nami/typedef.h>
 
+// Preprocessor definitions
+#define NAMI_PROMPT_DEFAULT "nami > "
+
+// Platform dependent macros
+#ifdef _WIN64
+#define DLLEXPORT extern __declspec(dllexport)
+#else
+#define DLLEXPORT
+#endif
+
+// Set the reallocator for the dict submodule
+#ifdef DICT_REALLOC
+    #undef DICT_REALLOC
+    #define DICT_REALLOC(p, sz) realloc(p, sz)
+#endif
+
+// Set the reallocator for the array submodule
+#ifdef ARRAY_REALLOC
+    #undef ARRAY_REALLOC
+    #define ARRAY_REALLOC(p, sz) realloc(p, sz)
+#endif
+
+// Set the reallocator for the json submodule
+#ifdef JSON_REALLOC
+    #undef JSON_REALLOC
+    #define JSON_REALLOC(p, sz) realloc(p, sz)
+#endif
+
+// Memory management macro
+#ifndef NAMI_REALLOC
+    #define NAMI_REALLOC(p, sz) realloc(p,sz)
+    #define NAMI_FREE(p) free(p)
+#endif
