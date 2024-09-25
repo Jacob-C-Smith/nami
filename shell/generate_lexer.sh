188c4c4252a551373dd2daac63018b6dc8879e2e
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-09-24 22:44:01 -0600

    [shell] Updated lexer generator

--- shell/generate_lexer.sh
+++ shell/generate_lexer.sh
@@ -23,3 +23,7 @@ cat resources/lexer/keywords | ./build/hash_optimal crc >> include/nami/lexer_lu
 # Construct the operators lookup table
 printf "const char *_p_operators" >> include/nami/lexer_lut.h
 cat resources/lexer/operators | ./build/hash_optimal crc >> include/nami/lexer_lut.h
+
+# Construct the separators lookup table
+printf "const char *_p_separators" >> include/nami/lexer_lut.h
+cat resources/lexer/separators | ./build/hash_optimal crc >> include/nami/lexer_lut.h
